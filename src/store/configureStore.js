67e6469dc68fa0d34dabb4803c4e2cabb2315c66
ZhaoFeng Zeng <zhao@ZhaoFengs-MacBook-Pro.local>
2018-01-23 01:05:44 -0800

    added reselect and redux-devtool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,11 +3,14 @@ import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
 const configureStore = (preloadedState) => {
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 	const store = createStore(
 		rootReducer,
-		preloadedState,
-		applyMiddleware(thunk)
-	);
+		/* preloadedState, */
+		composeEnhancers(
+			applyMiddleware(thunk)
+	));
 
 	if (module.hot) {
 		module.hot.accept("../reducers", () => {
